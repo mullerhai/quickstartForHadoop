e79a3628cecf088fcd33ce2895234328f7e833b6
linkedmemuller <hai710459649@foxmail.com>
2017-09-20 16:23:59 +0800

    maven 执行 shell 脚本经过修改已经可以完美实现在本地运行 查看 hadoop执行效果

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,3 +1,31 @@
-echo "run hadoop task"
-sudo su hadoop
-/usr/local/hadoop/hadoop-2.8.1/bin/hadoop jar /hadoopJars/quickstart-1.0-SNAPSHOT.jar HadoopStart   /input/*  /output/
+#!/bin/sh
+
+echo "run script is running"
+
+sleep 1
+APP_NAME=quickstart
+echo -e  'application: $APP_NAME'
+mkdir -p /hadoopJars/$APP_NAME
+sudo -i chown -R hadoop:hadoop /hadoopJars/$APP_NAME
+echo "ok change the application directory user to :hadoop"
+sudo -i chmod o+rw  /hadoopJars/$APP_NAME
+JAR=quickstart.jar
+cd  /hadoopJars
+mv ./$JAR     /hadoopJars/$APP_NAME
+echo "ok move run.sh to the appliction  directory "
+mv ./run.sh   /hadoopJars/$APP_NAME
+
+sleep 3
+
+echo "run task.sh"
+HADOOP_BIN_PATH=/usr/local/hadoop-2.8.1/bin/hadoop
+
+JARPATH=/hadoopJars/$APP_NAME/$JAR
+MAIN_CLASS=HadoopStart
+INPUT_DIR=/testdata/
+OUTPUT_DIR=/testouttmpsd
+
+sleep 2
+echo "task is running jar"
+
+sudo su hadoop  $HADOOP_BIN_PATH   jar    $JARPATH   $MAIN_CLASS  $INPUT_DIR  $OUTPUT_DIR
